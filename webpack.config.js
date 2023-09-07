0015a2971d14a1920779deca04e90d0f7f1cc9d4
Vladyslav Omelchenko <youchallengebread@gmail.com>
2023-09-07 13:45:04 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,18 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    entry: "./src/index.js",
-    mode: "development",
-    output: {
-        filename: "build.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    watch: true,
-    watchOptions: {
-        aggregateTimeout: 200,
-        ignored: /node_modules/, 
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
-    },
+  entry: './src/index.js',
+  mode: 'production',
+  output: {
+    filename: 'build.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
